d879b13ecd22be97c199bfa902f6ee5fce593e69
Matthijs Luijten <m.h.luijten@student.ru.nl>
2024-02-09 15:48:27 +0100

    first attempt in making the environment and pygame setup

--- main.py
+++ main.py
@@ -4,17 +4,38 @@ import pygame
 from src.environment.environment import Environment
 
 pygame.init()
-screen_size = (800, 600)  # Width x Height
+screen_size = (800, 600)
 screen = pygame.display.set_mode(size=screen_size)
 pygame.display.set_caption("RL dodge game")
-env = Environment(screen=screen, render_on=True)
-
 clock = pygame.time.Clock()
 
-while True:
-    env.render()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            pygame.quit()
-            sys.exit()
-    clock.tick(60)  # Limit frame rate to 60 FPS
+env = Environment(screen=screen, render_on=True)
+
+episodes = 5000
+for episode in range(episodes):
+    state = env.reset()
+    done = False
+
+    while not done:
+        env.render()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (
+                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+            ):
+                pygame.quit()
+                sys.exit()
+
+            # TODO:
+            # get agent action; action = agent.get_action(state)
+            # reward, next_state, done = env.step(action)
+
+            # if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+            #     prev_location = env.agent_location
+            #     env.agent_location = (
+            #         prev_location[0] + 0,
+            #         prev_location[1] + 1,
+            #     )
+        clock.tick(60)  # Limit frame rate to 60 FPS
+
+pygame.quit()
+sys.exit()
