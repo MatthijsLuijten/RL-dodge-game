68cd44d6be045127b34e3543137236ba2df6abd4
daphne <d.smits@sobolt.com>
2024-02-18 14:04:18 +0100

    Implement move, render and reset functionality player

--- src/agent.py
+++ src/agent.py
@@ -1,17 +1,43 @@
 from typing import Tuple
+import pygame
 
 
 class Agent:
-    def __init__(self, start_location: Tuple[int, int], size: int) -> None:
+    def __init__(
+        self, start_location: Tuple[int, int], size: int, color: Tuple[int, int, int]
+    ) -> None:
+        """Intializes an agent in the game.
+
+        Args:
+            start_location (Tuple[int, int]): The initial location of the agent.
+            size (int): The size of the agent (radius).
+            color (Tuple[int, int, int]): The color of the agent.
+        """
         self.start_location = start_location
         self.location = start_location
         self.size = size
+        self.color = color
 
-    def reset(self):
+    def reset(self) -> None:
+        """Reset the agent to its initial state."""
         pass
 
-    def render(self):
-        pass
+    def render(self, screen: pygame.Surface) -> pygame.Rect:
+        """Render the agent as a circle on the screen.
 
-    def move(self):
-        pass
+        Args:
+            screen (pygame.Surface): The surface to render the agent on.
+
+        Returns:
+            pygame.Rect: the created circle.
+        """
+        return pygame.draw.circle(screen, self.color, self.location, self.size)
+
+    def move(self, move: Tuple[int, int]) -> None:
+        """Move the agent by a specified amount.
+
+        Args:
+            move (Tuple[int, int]): The amount to move the agent in (x, y) direction.
+
+        """
+        self.location = (self.location[0] + move[0], self.location[1] + move[1])
--- src/player.py
+++ src/player.py
@@ -1,10 +1,46 @@
 from typing import Tuple
-from agent import Agent
+import pygame
+
+from src.agent import Agent
 
 
 class Player(Agent):
-    def __init__(self, start_location: Tuple[int, int], size: int = 10) -> None:
-        super().__init__(start_location, size)
+    def __init__(
+        self,
+        start_location: Tuple[int, int],
+        size: int = 10,
+    ) -> None:
+        """Intitalizes the Player with start location, size, and default color (white).
+
+        Args:
+            start_location (Tuple[int, int]): The initial location of the player.
+            size (int, optional): The size of the player (radius). Defaults to 10 pixels.
+        """
+        super().__init__(start_location, size, (255, 255, 255))
+
+        self.actions = {
+            0: (0, -1),  # Up
+            1: (0, 1),  # Down
+            2: (-1, 0),  # Left
+            3: (1, 0),  # Right
+        }
 
     def reset(self):
+        """Reset the player to its initial state."""
         self.location = self.start_location
+
+    def render(self, screen: pygame.Surface) -> None:
+        """Render the player on the screen.
+
+        Args:
+            screen (pygame.Surface): The surface to render the player on.
+        """
+        return super().render(screen)
+
+    def move(self, move: Tuple[int, int]):
+        """Move the player based on the given move.
+
+        Args:
+            move (Tuple[int, int]): Move to perform.
+        """
+        super().move(move)
